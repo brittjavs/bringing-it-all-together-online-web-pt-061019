0e6aba938320bcb194b543a6705417e5004a2385
Brittany Javalera <brittjavalera@gmail.com>
2019-08-22 03:18:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -69,7 +69,7 @@ attr_accessor :name, :breed, :id
     
   def self.find_by_name(name)
     sql = <<-SQL
-      SELECT * FROM dogs WHERE name = ?
+      SELECT * FROM dogs WHERE name = ? LIMIT 1
     SQL
 
     DB[:conn].execute(sql, name).map do |row|
