989cd386eb95c513d0f0f6ce0315515c3e3a746c
Brittany Javalera <brittjavalera@gmail.com>
2019-08-22 03:19:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,8 +63,9 @@ attr_accessor :name, :breed, :id
       dog_data = dog[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
-      song = self.create(name: name, breed: breed)
+      dog = self.create(name: name, breed: breed)
     end
+    dog
   end
     
   def self.find_by_name(name)
